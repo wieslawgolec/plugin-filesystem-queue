6e349f0c1911f21be9bfd43ec9340cbaebf3341b
Wieslaw Golec <wieslawgolec@crazysales.com.au>
2026-03-19 21:04:51 +0800

    Add supervisor tuning parameters to config

--- Config/config.php
+++ Config/config.php
@@ -17,5 +17,23 @@ return [
         'filesystem_queue_hit_time_limit' => null,
         'filesystem_queue_failed_msg_limit' => null,
         'filesystem_queue_failed_time_limit' => null,
+    
+        // Supervisor tuning parameters (used by mautic:emails:supervisor)
+        'filesystem_queue_supervisor_max_threads'                  => 8,
+        'filesystem_queue_supervisor_initial_threads'              => 2,
+        'filesystem_queue_supervisor_min_messages_per_thread'      => 10,
+        'filesystem_queue_supervisor_time_limit'                   => 300,           // seconds per thread
+        'filesystem_queue_supervisor_memory_limit'                 => '256M',
+        'filesystem_queue_supervisor_max_server_load'              => 4.0,
+        'filesystem_queue_supervisor_max_memory_usage_percent'     => 80,
+        'filesystem_queue_supervisor_delay_between_threads'        => 5,            // seconds
+        'filesystem_queue_supervisor_max_load_increase'            => 0.5,
+        'filesystem_queue_supervisor_max_mem_increase_percent'     => 5,
+        'filesystem_queue_supervisor_check_interval'               => 10,           // seconds
+        'filesystem_queue_supervisor_logging_enabled'              => true,
+        'filesystem_queue_supervisor_custom_log_name'              => '',           // leave empty for default: mautic_filesystem_queue_supervisor.log
+        'filesystem_queue_supervisor_benchmark_enabled'            => false,
+        'filesystem_queue_supervisor_verbosity_level'              => 0,            // 0 = none, 1 = -v, 2 = -vv, 3 = -vvv
+        'filesystem_queue_supervisor_check_maintenance_locks'      => false,    
     ],
 ];
